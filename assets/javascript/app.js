897264c066d6367b46748dc12f202566d81e3c85
Brett Porter <gkdesign06@gmail.com>
2019-10-02 17:59:21 -0500

    add all files for needed

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+// GLOBAL VARIABLES
+let topics = ['forrest Gump',]
+
+
+
+// pull default topics within the array
+var showDefaultButtons = function(arr){
+    for (var i = 0; i < arr.length;i++){
+        var buttonTopic = arr[i];
+        const btn = $('<div>');
+
+    }
+};
+
+// push user input to topic array and display gifs
+
+
+
