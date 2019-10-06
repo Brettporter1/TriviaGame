57f46784a3b7288f47c52b5d3e8565d2eb002f47
Brett Porter <gkdesign06@gmail.com>
2019-10-06 15:16:03 -0500

    added style to game and added timeouts after each answer selection so the player can either manually move go to the next question or it will automatically do it after a set time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,104 +1,89 @@
-// GLOBAL VARIABLES
-var questions = [
-    {
-        question: 'Which Musician did Jimi not cover during his career?',
-        answers: ['Elvis Presley', 'Bob Dylan', 'Chuck Berry', 'BB King'],
-        correctAnswer: 'BB King'
-    },
-    {
-        question: 'For who\'s band did Jimi play rhythm guitar?',
-        answers: ['Little Richard', 'Eric Clapton', 'Stevie Wonder','Muddy Waters'],
-        correctAnswer: 'Little Richard'
-    },
-    {
-        question: 'In what year did Jimi\'s solo career start?',
-        answers: ['1964', '1965','1966','1967'],
-        correctAnswer: '1966'
-    },
-    {
-        question: 'At what concert did Jimi infamously burn his guitar?',
-        answers: ['Filmore East', 'Monterey Pop Festival', 'Woodstock', 'Isle of Wight Festival'],
-        correctAnswer: 'Monterey Pop Festival'
-    },
-    {
-        question: 'What was Jimi\'s full name when he was born?',
-        answers: ['James Marshall Hendrix', 'Jimmy Marvin Hendrix', 'Johnny Allen Hendrix', 'James Allen Hendrix'],
-        correctAnswer: 'Johnny Allen Hendrix'
-    },
-    {
-        question: 'How many studio albums did Jimi release',
-        answers: ['3','2', '4', '7'],
-        correctAnswer: '3'
-    },
-    {
-        question: 'Which of these is not one of Jimi\'s bands?',
-        answers: ['Jimmy James and the Blue Flames', 'The Velvatones', 'Band of Gypsys', 'The Jimi Hendrix Experiment'],
-        correctAnswer: 'The Jimi Hendrix Experiment'
-    },
-    {
-        question: '',
-        answers: [],
-        correctAnswer: ''
-    },
-    {
-        question: '',
-        answers: [],
-        correctAnswer:''
-    },
-    {
-        question: '',
-        answers: [],
-        correctAnswer: ''
-    }
-];
-var time = 10;
-var loadedQuestion = '';
-var selectedAnswer = '';
-var correctAnswers = 0;
-var wrongAnswers = 0;
-var timeCount = function(){
-    time--;
-    $('#timer').html(time);
-    if(time <= 0){
-        clearInterval(timer);
-        console.log('Your Time is Up');
-        gameOver();
-    }
-}
+// // GLOBAL VARIABLES
+// var questions = [
+//     {
+//         question: 'Which of these is not one of Jimi\'s bands?',
+//         answers: ['Jimmy James and the Blue Flames', 'The Velvatones', 'Band of Gypsys', 'The Jimi Hendrix Experiment'],
+//         correctAnswer: 'The Jimi Hendrix Experiment'
+//     },
+//     {
+//         question: 'For who\'s band did Jimi play rhythm guitar?',
+//         answers: ['Little Richard', 'Eric Clapton', 'Stevie Wonder','Muddy Waters'],
+//         correctAnswer: 'Little Richard'
+//     },
+//     {
+//         question: 'In what year did Jimi\'s solo career start?',
+//         answers: ['1964', '1965','1966','1967'],
+//         correctAnswer: '1966'
+//     },
+//     {
+//         question: 'At what concert did Jimi infamously burn his guitar?',
+//         answers: ['Filmore East', 'Monterey Pop Festival', 'Woodstock', 'Isle of Wight Festival'],
+//         correctAnswer: 'Monterey Pop Festival'
+//     },
+//     {
+//         question: 'What was Jimi\'s full name when he was born?',
+//         answers: ['James Marshall Hendrix', 'Jimmy Marvin Hendrix', 'Johnny Allen Hendrix', 'James Allen Hendrix'],
+//         correctAnswer: 'Johnny Allen Hendrix'
+//     },
+//     {
+//         question: 'How many studio albums did Jimi release',
+//         answers: ['3','2', '4', '7'],
+//         correctAnswer: '3'
+//     },
+//     {
+//         question: 'Who was the drummer for Jimi\'s band, The Band of Gypsys',
+//         answers: ['Mitch Mitchell', 'Noel Redding', 'Billy Cox', 'Buddy Miles'],
+//         correctAnswer: 'Buddy Miles'
+//     },
+// ];
+// var time = 30;
+// var loadedQuestion;
+// var selectedAnswer = '';
+// var correctAnswers = 0;
+// var wrongAnswers = 0;
+// var timeCount = function(){
+//     time--;
+//     $('#timer').html(time);
+//     if(time <= 0){
+//         clearInterval(timer);
+//         console.log('Your Time is Up');
+//         gameOver();
+//     }
+// }
 
-    // onclick event for start game, hides start screen and renders timer
-    $('#start-game').on('click', function () {
-        $('#start-game').remove();
-        timer = setInterval(timeCount,1000);
-        $('#gameHeader').append("<h2 id='timeTitle'>Time Remaining</h2>").append(`<h1 id="timer">${time}</h1>`)
-        for (var i = 0; i < questions.length;i++){
-            $('#gameScreen').append(`<h2>${questions[i].question}</h2>`);
-            for(var j = 0; j < questions[i].answers.length;j++){
-                console.log(questions[i].answers[j]);
-                $('#gameScreen').append(`<input type="radio" name="question${i}" value="${questions[i].answers[j]}">${questions[i].answers[j]}`);
-            }
-        }
-    });
-    function gameOver(){
-        for (var i = 0; i < questions.length; i++){
-            $.each($(`input[name=question${i}]:checked`),function(){
-                if($(this).val()=== questions[i].correctAnswer){
-                    correctAnswers++;
-                    console.log(`right:${correctAnswers}`);     
-                }
-                else{
-                    wrongAnswers++;
-                    console.log(`wrong:${wrongAnswers}`);
-                }
-                console.log(questions.length - (correctAnswers + wrongAnswers));
-            })
+//     // onclick event for start game, hides start screen and renders timer
+//     $('#start-game').on('click', function () {
+//         $('#start-game').remove();
+//         timer = setInterval(timeCount,1000);
+//         $('#gameHeader').append("<h2 id='timeTitle'>Time Remaining</h2>").append(`<h1 id="timer">${time}</h1>`)
+//         for (var i = 0; i < questions.length;i++){
+//             $('#gameScreen').append(`<h2>${questions[i].question}</h2>`);
+//             for(var j = 0; j < questions[i].answers.length;j++){
+//                 console.log(questions[i].answers[j]);
+//                 $('#gameScreen').append(`<input type="radio" name="question${i}" value="${questions[i].answers[j]}">${questions[i].answers[j]}`);
+//             }
+//         }
+//     });
+//     function gameOver(){
+//         for (var i = 0; i < questions.length; i++){
+//             $.each($(`input[name=question${i}]:checked`),function(){
+//                 if($(this).val()=== questions[i].correctAnswer){
+//                     correctAnswers++;
+//                     console.log(`right:${correctAnswers}`);     
+//                 }
+//                 else{
+//                     wrongAnswers++;
+//                     console.log(`wrong:${wrongAnswers}`);
+//                 }
+//                 console.log(questions.length - (correctAnswers + wrongAnswers));
+//             })
             
-        }
-        // write the results back to the DOM
-    }
+//         }
+//         // write the results back to the DOM
+//     }
 
 
 
-// loop through questions and write to DOM
+// // loop through questions and write to DOM
 
-// push user input to topic array and display gifs
+// // push user input to topic array and display gifs
--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -2,150 +2,184 @@
 
 var questions = [
     {
-        question: 'Which Musician did Jimi not cover during his career?',
-        answers: ['Elvis Presley', 'Bob Dylan', 'Chuck Berry', 'BB King'],
-        correctAnswer: 'BB King'
+        question: 'Which of these is not one of Jimi\'s bands?',
+        answers: ['Jimmy James and the Blue Flames', 'The Velvatones', 'Band of Gypsys', 'The Jimi Hendrix Experiment'],
+        correctAnswer: 'The Jimi Hendrix Experiment',
+        image:''
     },
     {
         question: 'For who\'s band did Jimi play rhythm guitar?',
-        answers: ['Little Richard', 'Eric Clapton', 'Stevie Wonder','Muddy Waters'],
-        correctAnswer: 'Little Richard'
+        answers: ['Little Richard', 'Eric Clapton', 'Stevie Wonder', 'Muddy Waters'],
+        correctAnswer: 'Little Richard',
+        image:''
     },
     {
         question: 'In what year did Jimi\'s solo career start?',
-        answers: ['1964', '1965','1966','1967'],
-        correctAnswer: '1966'
+        answers: ['1964', '1965', '1966', '1967'],
+        correctAnswer: '1966',
+        image:''
     },
     {
         question: 'At what concert did Jimi infamously burn his guitar?',
         answers: ['Filmore East', 'Monterey Pop Festival', 'Woodstock', 'Isle of Wight Festival'],
-        correctAnswer: 'Monterey Pop Festival'
+        correctAnswer: 'Monterey Pop Festival',
+        image:''
     },
     {
         question: 'What was Jimi\'s full name when he was born?',
         answers: ['James Marshall Hendrix', 'Jimmy Marvin Hendrix', 'Johnny Allen Hendrix', 'James Allen Hendrix'],
-        correctAnswer: 'Johnny Allen Hendrix'
+        correctAnswer: 'Johnny Allen Hendrix',
+        image:''
     },
     {
         question: 'How many studio albums did Jimi release',
-        answers: ['3','2', '4', '7'],
-        correctAnswer: '3'
+        answers: ['3', '2', '4', '7'],
+        correctAnswer: '3',
+        image:''
     },
     {
-        question: 'Which of these is not one of Jimi\'s bands?',
-        answers: ['Jimmy James and the Blue Flames', 'The Velvatones', 'Band of Gypsys', 'The Jimi Hendrix Experiment'],
-        correctAnswer: 'The Jimi Hendrix Experiment'
+        question: 'Who was the drummer for Jimi\'s band, The Band of Gypsys',
+        answers: ['Mitch Mitchell', 'Noel Redding', 'Billy Cox', 'Buddy Miles'],
+        correctAnswer: 'Buddy Miles'
     },
     {
         question: 'Before music, which branch of the military was Jimi in?',
         answers: ['Navy', 'Marines', 'Army', 'Airforce'],
-        correctAnswer: 'Army'
+        correctAnswer: 'Army',
+        image:''
     },
     {
         question: 'What was the model year of the Fender Jimi played at Woodstock?',
-        answers: ['1967','1968','1969', '1970'],
-        correctAnswer:'1968'
+        answers: ['1967', '1968', '1969', '1970'],
+        correctAnswer: '1968',
+        image:''
     },
     {
         question: 'In what city did Jimi pass away?',
-        answers: ['Paris','Seattle','New York','London'],
-        correctAnswer: 'London'
+        answers: ['Paris', 'Seattle', 'New York', 'London'],
+        correctAnswer: 'London',
+        image:''
     }
 ];
+// $(document).ready(function() {
+//     var audioElement = document.createElement("audio");
+//     audioElement.setAttribute("src", "./assets/audio/Jimi Hendrix - Hey Joe.mp3");
+//     audioElement.play();
+// });
+
 $('#start-game').on('click', function () {
     $('#start-game').remove();
+    $('#gameDescript').remove();
+    $('#gameScreen').addClass('questions');
     $('#timer').html(game.time);
     game.renderQuestion();
 });
-$(document).on('click','.trivia-button', function(event){
-    if(game.loadedQuestion !== questions.length - 1){
+$(document).on('click', '#reset',function(){
+    location.reload(true);
+})
+$(document).on('click', '.trivia-button', function (event) {
+    if (game.loadedQuestion !== questions.length - 1) {
         game.selectedAnswer(event);
     }
-    else{game.results()}
+    else { game.results() }
 });
-$(document).on('click','#nextQuest',function(){
+$(document).on('click', '#nextQuest', function () {
+    clearTimeout(timeoutNext);
     game.nextQuestion();
 })
-$(document).on('click','#result',function(){
+$(document).on('click', '#result', function () {
+    clearTimeout()
     game.results();
 })
-var game ={
-    questions:questions,
-    time : 10,
-    loadedQuestion : 0,
-    correctAnswers : 0,
-    wrongAnswers : 0,
-    timeCount : function(){
+var game = {
+    questions: questions,
+    time: 10,
+    loadedQuestion: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+    timeCount: function () {
         game.time--;
         $('#timer').html(game.time);
-        if(game.time <= 0){
+        if (game.time <= 0) {
             console.log('Your Time is Up');
             game.timesUp();
         }
-},
-renderQuestion: function(){
-    timer = setInterval(game.timeCount, 1000);
-    $('#gameScreen').html(`<h3 class="trivQuest">${questions[game.loadedQuestion].question}</h3>`);
-    for (var i = 0; i< questions[game.loadedQuestion].answers.length;i++){
-        var trivBtn = $('<button>');
-        trivBtn.addClass('trivia-button');
-        trivBtn.attr('id', `button-${i}`);
-        trivBtn.attr('data-name', questions[game.loadedQuestion].answers[i]);
-        trivBtn.text(questions[game.loadedQuestion].answers[i]);
-        $('#gameScreen').append(trivBtn);
+    },
+    renderQuestion: function () {
+        timer = setInterval(game.timeCount, 1000);
+        $('#gameScreen').html(`<h3 class="trivQuest">${questions[game.loadedQuestion].question}</h3>`);
+        for (var i = 0; i < questions[game.loadedQuestion].answers.length; i++) {
+            var trivBtn = $('<button>');
+            trivBtn.addClass('trivia-button');
+            trivBtn.attr('id', `button-${i}`);
+            trivBtn.attr('data-name', questions[game.loadedQuestion].answers[i]);
+            trivBtn.text(questions[game.loadedQuestion].answers[i]);
+            $('#gameScreen').append(trivBtn);
+        }
+    },
+    selectedAnswer: function (event) {
+        clearInterval(timer);
+        if ($(event.target).data('name').toString() === questions[game.loadedQuestion].correctAnswer.toString()) {
+            $(event.target).addClass('correct');
+            game.rightAnswer();
+        }
+        else {
+            $(event.target).addClass('wrong');
+            game.wrongAnswer();
+        }
+    },
+    nextQuestion: function () {
+        game.time = 10;
+        $('#timer').html(game.time);
+        game.loadedQuestion++;
+        game.renderQuestion();
+    },
+    timesUp: function () {
+        clearInterval(timer);
+        game.wrongAnswers++;
+        $('#gameScreen').html(`<h2 id="time-up-comment">THE CORRECT ANSWER WAS: ${questions[game.loadedQuestion].correctAnswer.toString()}`);
+        if (game.loadedQuestion === questions.length - 1) {
+            timeoutResults = setTimeout(game.results, 5*1000);
+            $('#gameScreen').append('<button id="result">See Results</button>');
+        }
+        else {
+            timeoutNext = setTimeout(game.nextQuestion, 5*1000);
+            $('#gameScreen').append('<button id="nextQuest">Next Question</button>');
+        }
+        
+    },
+    rightAnswer: function () {
+        console.log('correct');
+        game.correctAnswers++;
+        if (game.loadedQuestion === questions.length - 1) {
+            // $('#gameScreen').append(`<img class="answer-image" src="${questions[game.loadedQuestion].image}">`);
+            timeoutResults = setTimeout(game.results, 5*1000);
+        }
+        else {
+            timeoutNext = setTimeout(game.nextQuestion, 5*1000);
+            // $('#gameScreen').append(`<img class="answer-image" src="${questions[game.loadedQuestion].image}">`);
+            $('#gameScreen').append('<button id="nextQuest">Next Question</button>');
+        };
+    },
+    wrongAnswer: function () {
+        console.log('wrong');
+        game.wrongAnswers++;
+        if (game.loadedQuestion === questions.length - 1) {
+            timeoutResults = setTimeout(game.results, 5*1000);
+            $('#gameScreen').html(`<h2 id="time-up-comment">CORRECT ANSWER WAS: ${questions[game.loadedQuestion].correctAnswer.toString()}`);
+            // $('#gameScreen').append(`<img class="answer-image" src="${questions[game.loadedQuestion].image}">`);
+            
+        }
+        else {
+            timeoutNext = setTimeout(game.nextQuestion, 5*1000);
+            $('#gameScreen').append(`<h2 id="time-up-comment">CORRECT ANSWER WAS: ${questions[game.loadedQuestion].correctAnswer.toString()}`);
+            $('#gameScreen').append('<button id="nextQuest">Next Question</button>');
+        };
+    },
+    results: function () {
+        clearInterval(timer);
+        $('#timer').empty();
+        $('#gameScreen').html(`<h1 id="winCount">Correct Answers: ${game.correctAnswers}</h1>`).append(`<h1 id="winCount">Wrong Answers: ${game.wrongAnswers}</h1>`).append('<button id="reset">Restart</button>');
+        
     }
-},
-selectedAnswer: function(event){
-    clearInterval(timer);
-    if($(event.target).data('name').toString() === questions[game.loadedQuestion].correctAnswer.toString()){
-        $(event.target).addClass('correct');
-        game.rightAnswer();
-    }
-    else{
-        $(event.target).addClass('wrong');
-        game.wrongAnswer();
-    }
-},
-nextQuestion: function(){
-    game.time = 10;
-    $('#timer').html(game.time);
-    game.loadedQuestion++;
-    game.renderQuestion();
-},
-timesUp: function(){
-    clearInterval(timer);
-    $('#gameScreen').html(`<h2 id="time-up-comment">THE CORRECT ANSWER WAS: ${questions[game.loadedQuestion].correctAnswer.toString()}`);
-    if(game.loadedQuestion === questions.length - 1){
-        $('#gameScreen').append('<button id="result">See Results</button>');
-    }
-    else{
-        $('#gameScreen').append('<button id="nextQuest">Next Question</button>');
-    }
-    game.wrongAnswers++;
-},
-rightAnswer: function(){
-    console.log('correct');
-    game.correctAnswers++;
-    if(game.loadedQuestion === questions.length - 1){
-        setTimeout(game.results, 2000);
-    }
-    else{
-       $('#gameScreen').append('<button id="nextQuest">Next Question</button>');
-    };
-},
-wrongAnswer: function(){
-    console.log('wrong');
-    game.wrongAnswers++;
-    if(game.loadedQuestion === questions.length - 1){
-        setTimeout(game.results, 2000);
-    }
-    else{
-       $('#gameScreen').append('<button id="nextQuest">Next Question</button>');
-    };
-},
-results: function(){
-    clearInterval(timer);
-    $('#timer').empty();
-    $('#gameScreen').html(`<h1 id="winCount">Correct Answers: ${game.correctAnswers}</h1>`).append(`<h1 id="winCount">Wrong Answers: ${game.wrongAnswers}</h1>`)
-}
 }
